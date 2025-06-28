5ec0ed6e85f4892dfbbf345a56ae7143b1489859
atul23g <atulya.login04@gmail.com>
2025-06-28 15:27:02 +0530

    Fix Vercel build: Convert to JS config and disable ESLint completely

---
+++ next.config.js
@@ -0,0 +1,43 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // ⚠️ Turns every ESLint error into a build warning
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placeimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
